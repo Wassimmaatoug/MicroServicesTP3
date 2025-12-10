88ad2904211c3c2b6dc545d19123cad2910e02e5
Wassimmaatougue <167562690+Wassimmaatoug@users.noreply.github.com>
2025-12-10 22:50:39 +0100

    Delete Calculatrice-Service directory

--- Calculatrice-Service/src/main/java/master/fss/calculatriceservice/controller/CalculatriceController.java
+++
@@ -1,4 +0,0 @@
-package master.fss.calculatriceservice.controller;
-
-public class CalculatriceController {
-}
--- Calculatrice-Service/src/main/java/master/fss/calculatriceservice/metier/ICalculatrice.java
+++
@@ -1,4 +0,0 @@
-package master.fss.calculatriceservice.metier;
-
-public interface ICalculatrice {
-}
